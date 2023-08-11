6b431e0bc42369281b3f2476f751569a0204ad1e
VedaIT2023 <vedait2023@gmail.com>
2023-08-11 11:21:20 +0530

    Opps project added addition class added

---
+++ OOPSConcepts/src/com/y4j/maths/additions/AdditionsConcepts.java
@@ -0,0 +1,18 @@
+package com.y4j.maths.additions;
+
+public class AdditionsConcepts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Addition");
+		additionOfTwoNumbers();
+	}
+	
+	public static void additionOfTwoNumbers() {
+		
+		int a = 10, b = 20;
+		int c = a + b;
+		System.out.println("Addition of 2 numbers is " + c);
+	}
+
+}
